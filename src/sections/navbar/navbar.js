c80d33ce9d130c45f0692f666954bdcde7f6561a
PrivilegeMendes <20317699+PrivilegeMendes@users.noreply.github.com>
2021-05-15 12:16:45 +0200

    publish portfolio to Github Pages

--- src/sections/navbar/navbar.js
+++ src/sections/navbar/navbar.js
@@ -2,29 +2,29 @@ import React from 'react';
 import './navbar.css';
 
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function () { myFunction() };
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
 
 const Navbar = () => {
     return (
         <nav className="bg-blue">
             <div className="dt w-100 mw8 center">
-                <div className="dtc v-mid tr pa3" id="navbar">
+                <div className="dtc v-mid tr pa3">
                     <a className="f5 fw4 hover-white no-underline white-80 dib dn dib-l pv2 ph3" href="/" >About</a>
                     <a className="f5 fw4 hover-white no-underline white-80 dib dib-l pv2 ph3" href="/" >Resume</a>
                     <a className="f5 fw4 hover-white no-underline white-80 dib dib-l pv2 ph3" href="/" >Projects</a>
